7597ccc958cfb79447f8053a04c8c1fbcb6ce6d7
Jason Mullings <wjasonmullings@gmail.com>
2020-06-07 21:29:30 -0400

    palindrome.js done with red green tests.

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,14 @@
 module.exports = (str) => {
-  return [str];
-  };
+  let ans = [];
+  let sentence = str.replace(/\s/g, '').toLowerCase()
+  let forward = sentence.split('')
+  let backward = sentence.split('').reverse()
+  
+forward[0] === backward[0] || forward[0] === backward[1]?
+    ans.push(forward.join('').replace(/[,!.*+\-?^${}()|[\]\\]/g,""))
+  :
+  ans
+  
+  return ans
+
+};
